3b190c64cfefb55b879f29b455693d2fe559c342
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-09-25 17:24:29 +0700

    set connect db use typeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from '../ormconfig';
 
+@Global()
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
