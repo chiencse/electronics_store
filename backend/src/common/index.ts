6b59a4e50a4e75c9ba9e506dcf14ae1334dab564
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-10-13 22:01:48 +0700

    create decorator, baseEntity

--- backend/src/common/index.ts
+++ backend/src/common/index.ts
@@ -1,3 +1,4 @@
-export * from './genSalt';
+export * from './Entity/baseEntity';
 export * from './http-exeption';
 export * from './typeorm-exeption.filter';
+export * from './decorator/current_user';
