97fbd02cc249ace965889964b357c220aec38a2a
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-10-19 10:09:39 +0700

    update something

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -20,7 +20,7 @@ import {
     ApiTags,
 } from '@nestjs/swagger';
 import { SignInDto } from './dto/signIn.dto';
-import { CurrentUser } from 'src/common';
+import { CurrentUser } from '../common';
 import { AuthPayload } from './entities/user.entity';
 import { AuthGuard } from './guard/jwt.guard';
 
