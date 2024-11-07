9b3f913fd30291ec19d0c7415446564353a3cc8f
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-11-07 22:24:59 +0700

    Setup send email veryfied code and implement forgot password user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,6 +13,7 @@ import { UserModule } from './user/user.module';
 import { FilesAzureService } from './modules/files/files.service';
 import { AuthModule } from './auth/auth.module';
 import { CurrentUserMiddleware } from 'ultility/middleware/current-user.middleware';
+import { MailModule } from './mail/mail.module';
 
 @Global()
 @Module({
@@ -24,15 +25,16 @@ import { CurrentUserMiddleware } from 'ultility/middleware/current-user.middlewa
         }),
         AuthModule,
         UserModule,
+        MailModule,
     ],
     controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer.apply(CurrentUserMiddleware).forRoutes({
-            path: '*',
-            method: RequestMethod.ALL,
-        }); // áp dụng cho tất cả các route trong module
-    }
+    // configure(consumer: MiddlewareConsumer) {
+    //     consumer.apply(CurrentUserMiddleware).forRoutes({
+    //         path: '*',
+    //         method: RequestMethod.ALL,
+    //     }); // áp dụng cho tất cả các route trong module
+    // }
 }
---
+++ backend/src/mail/mail.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailController } from './mail.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+
+@Module({
+
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        }
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>',
+      },
+      
+    }
+    )
+  ],
+
+  controllers: [MailController],
+  providers: [MailService],
+})
+export class MailModule {}
