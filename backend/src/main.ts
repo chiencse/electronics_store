8fdaabdbf39bef6ca8f6bfb514e7bb3b2277db78
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-11-07 15:31:32 +0700

    update port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3001;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
@@ -33,6 +35,6 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-    await app.listen(3001);
+    await app.listen(PORT);
 }
 bootstrap();
